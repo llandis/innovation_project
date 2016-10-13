de2bc1e545c5b1a503cc3842607de27048c4bdf1
Harry Liu <hfliu@ucdavis.edu>
2016-10-12 20:32:08 -0700

    Edited to include top level declarations.
    Won't need some declarations as we create a top level .v file.

--- seg_disp.v
+++ seg_disp.v
@@ -1,11 +1,14 @@
 module seg_disp
 (
+	input KEY0, KEY1, KEY2, KEY3, 
+	input [6:0] SW,
+	output [0:6] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6,
+	output [0:9] LEDR
+);
+	
 	//seq_num,
 	//freq_num,
 	//rom_addr
-);
-	input [6:0] SW;
-	output [0:6] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6;
 	
 	bcd_conv seq(SW[6:0], HEX0, HEX1);
 	
