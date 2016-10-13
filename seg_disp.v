efabde13119ae85eb0a66e7f60a67a91af42d9d9
Harry Liu <hfliu@ucdavis.edu>
2016-10-12 19:56:23 -0700

    Updated to add possible arguments and then call the bcd converter.

--- seg_disp.v
+++ seg_disp.v
@@ -1,8 +1,12 @@
 module seg_disp
 (
-  input [9:0] SW,
-  output [0:6] HEX0, HEX1, HEX2, HEX3,
-  output [0:9] LEDR
+	//seq_num,
+	//freq_num,
+	//rom_addr
 );
- 
+	input [6:0] x, SW;
+	output [0:6] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6;
+	
+	bcd_conv seq(SW[6:0], HEX0, HEX1);
+	
 endmodule
