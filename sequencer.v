f665eeb2b136319fcc2c0346ac7e45b8a0986023
alohastitch12 <kkita12@gmail.com>
2016-10-12 21:14:27 -0700

    Update sequencer.v

--- sequencer.v
+++ sequencer.v
@@ -21,7 +21,24 @@ module sequencer(
     end
   end
 );
+endmodule
 
+module debouncer (noisy,clk_1KHz,debounced);
 
+input wire clk_1KHz, noisy;
+output reg debounced;
+
+reg [7:0] reg;
+
+//reg: wait for stable
+always @ (posedge clk_1KHz) 
+begin
+reg[7:0] <= {reg[6:0],noisy}; //shift register
+if(reg[7:0] == 8'b00000000)
+  debounced <= 1'b0;
+else if(reg[7:0] == 8'b11111111)
+  debounced <= 1'b1;
+else debounced <= debounced;
+end
 
 endmodule
