9bc4847a12849ae8b90720ce8c5beea69ed5a2a6
alohastitch12 <kkita12@gmail.com>
2016-10-12 20:59:54 -0700

    Update sequencer.v

--- sequencer.v
+++ sequencer.v
@@ -1,13 +1,25 @@
 module sequencer(
-input pb_seq_up,
-input pb_seq_dn,
-input slow_clk,
-input clk_50,
-input reset,
+  input pb_seq_up,
+  input pb_seq_dn,
+  input slow_clk,
+  input clk_50,
+  input reset,
 
-output ROM_addr,
-output seq_num,
-output LEDS,
+  output ROM_addr,
+  output seq_num,
+  output LEDS,
+ initial begin
+   // seq_num = 0;
+    if (pb_seq_up == 1 & pb_seq_dn == 0) begin
+      seq_num = seq_num + 1;
+    end
+    else if (pb_seq_up == 0 & pb_seq_dn == 1) begin
+      seq_num = seq_num - 1;
+    end
+    else if (pb_seq_up == 0 & pb_seq_dn ==0) begin
+      seq_num = seq_num
+    end
+  end
 );
 
 
