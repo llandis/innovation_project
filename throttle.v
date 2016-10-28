084308b4899d53649360a5f2dc42257260975b9f
gheidkamp11 <g.heidkamp11@gmail.com>
2016-10-27 19:36:49 -0700

    Update throttle.v

--- throttle.v
+++ throttle.v
@@ -53,3 +53,23 @@ end
 endmodule
 
 
+module debouncer (noisy,clk_50,debounced);
+
+input wire clk_50, noisy;
+output reg debounced;
+
+reg [7:0] reg;
+
+//reg: wait for stable
+  always @ (posedge clk_50) 
+begin
+reg[7:0] <= {reg[6:0],noisy}; //shift register
+if(reg[7:0] == 8'b00000000)
+  debounced <= 1'b0;
+else if(reg[7:0] == 8'b11111111)
+  debounced <= 1'b1;
+else debounced <= debounced;
+end
+
+endmodule
+
