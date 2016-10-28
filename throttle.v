f65c51b76c713b02f7f2a4c489250a6231a2a4ea
gheidkamp11 <g.heidkamp11@gmail.com>
2016-10-27 19:30:43 -0700

    Update throttle.v

--- throttle.v
+++ throttle.v
@@ -9,26 +9,46 @@ input pb_freq_dn,
 output slow_clk,
 output freq_num,
 );
+wire dbUP
+wire dbDN
 
-	
+debouncer (pb_freq_up,CLK_50,dbUP);
+debouncer (pb_freq_dn,CLK_50,dbDN);
+
 always @(posedge CLK_50)
 begin
-	if (pb_freq_up == 1 & pb_freq_dn == 0)
-    		begin
-     		 freq_num = freq_num +1;
-    		end
-	else if (pb_freq_up ==0 & pb_freq_dn == 1)
+	if (reset == 1)
 		begin 
-		 freq_num = freq_num - 1; 
+		freq_num = 0; 
 		end
-	else if (pb_freq_up == 1 & pb_freq_dn == 1)
-		begin
-		 freq_num = freq_num;
-		end
-	else (pb_freq_up == 0 & pb_freq_dm == 0)
+	else 
 		begin
 		freq_num = freq_num;
 		end
+	if (freq_num = 0 || freq_num=5)
+         	begin
+		freq_num=freq_num;
+		end	
+	else 
+		begin
+		if (dbUP == 1 & dbDN == 0)
+    			begin
+     			 freq_num = freq_num +1;
+    			end
+		else if (dbUP ==0 & dbDN == 1)
+			begin 
+		 	freq_num = freq_num - 1; 
+			end
+		else if (dbUP == 1 & dbDN == 1)
+			begin
+			 freq_num = freq_num;
+			end
+		else 
+			begin
+			freq_num = freq_num;
+			end
+		end
+	
 end
 endmodule
 
