80399936c8b440f6c095810e4729bfdfbe1a916e
llandis <lawrence.landis@intel.com>
2016-10-27 19:58:48 -0700

    Delete ROM2Port_inst.v

--- ROM2Port_inst.v
+++
@@ -1,8 +0,0 @@
-ROM2Port	ROM2Port_inst (
-	.address_a ( address_a_sig ),
-	.address_b ( address_b_sig ),
-	.clock_a ( clock_a_sig ),
-	.clock_b ( clock_b_sig ),
-	.q_a ( q_a_sig ),
-	.q_b ( q_b_sig )
-	);
