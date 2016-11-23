8ff68e32362e9ae368097047d28cfe39ee5d322d
gheidkamp11 <g.heidkamp11@gmail.com>
2016-11-22 20:46:33 -0800

    Add files via upload

--- tb_throttle.v
+++ tb_throttle.v
@@ -1,54 +1,61 @@
-`timescale 1ns / 1ns
 module tb_throttle;
 
-reg CLK_50;
-reg reset;
-reg pb_freq_up;
-reg pb_freq_dn;
-wire slow_clk;
-wire freq_num;
-
-throttle DUT (
-  .CLK_50(CLK_50),
-  .reset(reset),
-  .pb_freq_up(pb_freq_up),
-  .pb_freq_dn(pb_freq_dn),
-  .slow_clk(slow_clk),
-  .freq_num(freq_num));
-
-initial begin
-  CLK_50 = 0;
-  forever #10 CLK_50 = ~CLK_50;
-end
-
-initial begin
-  reset = 0;
-  pb_freq_up = 0;
-  pb_freq_dn = 0;
-  #20 reset = 1;
-  #20 reset = 0;
-  #10000 pb_freq_up = 1;
-  #100; pb_freq_up = 0;
-  #10000 pb_freq_up = 1;
-  #100; pb_freq_up = 0;
-  #10000 pb_freq_dn = 1;
-  #100; pb_freq_dn = 0;
-  #10000 pb_freq_dn = 1;
-  #100; pb_freq_dn = 0;
-  #10000 pb_freq_up = 1;
-  #100 pb_freq_up = 0;
-  #10000 pb_freq_up = 1;
-  #100 pb_freq_up = 0;
-  #10000 pb_freq_dn = 1;
-  #100 pb_freq_dn = 0;
-  #10000 pb_freq_up = 1;
-  #100 pb_freq_up = 0;
-  #10000 pb_freq_up = 1;
-  #100 pb_freq_up = 0;
-  #10000 pb_freq_up = 1;
-  #100 pb_freq_up = 0;
-  #10000;
-$stop;
-end
-
+	reg CLK_50;
+	reg reset;
+	reg pb_freq_up;
+	reg pb_freq_dn;
+	wire slow_clk;
+	wire freq_num;
+	
+throttle throttle_DUT(
+	.CLK_50(CLK_50),
+	.reset(reset),
+	.pb_freq_up(pb_freq_up),
+	.pb_freq_dn(pb_freq_dn),
+	.slow_clk(slow_clk),
+	.freq_num(freq_num)
+);
+	
+	initial begin 
+		CLK_50 = 0;
+		forever #10 CLK_50 = ~CLK_50;
+	end
+		
+	initial begin
+		reset = 1;
+		pb_freq_up = 0; 
+		pb_freq_dn = 0;
+		#40 reset = 0;
+		
+		#60000 pb_freq_up = 1;
+		#200 pb_freq_up = 0;
+		#60000 pb_freq_up = 1;
+		#200 pb_freq_up = 0;
+		#60000 pb_freq_up = 1;
+		#200 pb_freq_up = 0;
+		#60000 pb_freq_up = 1;
+		#200 pb_freq_up = 0;
+		#60000 pb_freq_up = 1;
+		#200 pb_freq_up = 0;
+		#60000 pb_freq_up = 1;
+		#200 pb_freq_up = 0;
+		
+		#60000 pb_freq_dn = 1;
+		#200 pb_freq_dn = 0;
+		#60000 pb_freq_dn = 1;
+		#200 pb_freq_dn = 0;
+		#60000 pb_freq_dn = 1;
+		#200 pb_freq_dn = 0;
+		#60000 pb_freq_dn = 1;
+		#200 pb_freq_dn = 0;
+		#60000 pb_freq_dn = 1;
+		#200 pb_freq_dn = 0;
+		#60000 pb_freq_dn = 1;
+		#200 pb_freq_dn = 0;
+		$stop;
+		
+		end
 endmodule
+		
+	
+	
