4044990833046acfb972dd8e8be602c25131a3a8
gheidkamp11 <g.heidkamp11@gmail.com>
2016-11-30 20:52:06 -0800

    Update sequencer.v

--- sequencer.v
+++ sequencer.v
@@ -2,24 +2,21 @@ module sequencer(
   input pb_seq_up,
   input pb_seq_dn,
   input slow_clk,
-  input clk_50,
+  input CLK_50,
   input reset,
 
-  output rom_addr,
-  output seq_num
+  output [9:0] rom_addr,
+  output [5:0] seq_num
 );
   
-   always ( ) begin
-   // seq_num = 0;
-   // if (pb_seq_up == 1 & pb_seq_dn == 0) begin
-    //  seq_num = seq_num + 1;
-   // end
-   // else if (pb_seq_up == 0 & pb_seq_dn == 1) begin
-   //   seq_num = seq_num - 1;
-   // end
-   // else if (pb_seq_up == 0 & pb_seq_dn ==0) begin
-     // seq_num = seq_num
-   // end
+  always (@posedge CLK_50) begin
+    if (pb_seq_up == 1 & pb_seq_dn == 0)
+      seq_num <= seq_num + 1;
+    else if (pb_seq_up == 0 & pb_seq_dn == 1)
+      seq_num <= seq_num - 1;
+    else if (pb_seq_up == 0 & pb_seq_dn ==0)
+      seq_num <= seq_num;
   end
+  
 endmodule
 
