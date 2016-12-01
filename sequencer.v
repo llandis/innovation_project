3ff39c88fb4d4cdfe133843888539da5e86b1a4c
gheidkamp11 <g.heidkamp11@gmail.com>
2016-11-30 20:41:46 -0800

    Update sequencer.v

--- sequencer.v
+++ sequencer.v
@@ -6,8 +6,10 @@ module sequencer(
   input reset,
 
   output rom_addr,
-  output seq_num;
-  always ( ) begin
+  output seq_num
+);
+  
+   always ( ) begin
    // seq_num = 0;
    // if (pb_seq_up == 1 & pb_seq_dn == 0) begin
     //  seq_num = seq_num + 1;
@@ -19,25 +21,5 @@ module sequencer(
      // seq_num = seq_num
    // end
   end
-);
 endmodule
 
-module debouncer (noisy,clk_1KHz,debounced);
-
-input wire clk_1KHz, noisy;
-output reg debounced;
-
-reg [7:0] reg;
-
-//reg: wait for stable
-always @ (posedge clk_1KHz) 
-begin
-reg[7:0] <= {reg[6:0],noisy}; //shift register
-if(reg[7:0] == 8'b00000000)
-  debounced <= 1'b0;
-else if(reg[7:0] == 8'b11111111)
-  debounced <= 1'b1;
-else debounced <= debounced;
-end
-
-endmodule
