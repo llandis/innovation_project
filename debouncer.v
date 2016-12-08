61ed5658b61850f0f8d26518864973741b7b764f
Harry Liu <hfliu@ucdavis.edu>
2016-12-07 17:46:24 -0800

    newest updates.

--- debouncer.v
+++ debouncer.v
@@ -3,17 +3,17 @@ module debouncer (noisy,clk_50,debounced);
 input wire clk_50, noisy;
 output reg debounced;
 
-reg [7:0] reg;
+reg [7:0] reg1;
 
 //reg: wait for stable
   always @ (posedge clk_50) 
 begin
-reg[7:0] <= {reg[6:0],noisy}; //shift register
-if(reg[7:0] == 8'b00000000)
-  debounced <= 1'b0;
-else if(reg[7:0] == 8'b11111111)
-  debounced <= 1'b1;
-else debounced <= debounced;
+	reg1[7:0] <= {reg1[6:0],noisy}; //shift register
+	if(reg1[7:0] == 8'b00000000)
+	  debounced <= 1'b0;
+	else if(reg1[7:0] == 8'b11111111)
+	  debounced <= 1'b1;
+	else debounced <= debounced;
 end
 
 endmodule
--- seg_disp.v
+++ seg_disp.v
@@ -1,25 +1,22 @@
 module seg_disp
 (
-	//input KEY0, KEY1, KEY2, KEY3, 
-	input [6:0] SW,
-	output [0:6] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6
-	//output [0:9] LEDR
+	output [0:6] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5,
 	input [2:0] freq_num,
 	input [5:0] seq_num,
 	input [9:0] rom_addr
 );
 	//turn these into inputs as integrated into the top level module
-	//wire [6:0] seq_num;
-	//wire [6:0] freq_num;
-	//wire [6:0] rom_addr;
+	wire [9:0] seq_num_pass;
+	wire [9:0] freq_num_pass;
+	wire [9:0] rom_addr_pass;
 	wire [0:6] temp0, temp1, temp2;
 	
-	//assign freq_num = 7'b000_1001;
-	//assign seq_num = 7'b011_1111;
-	//assign rom_addr = 7'b111_1111;
+	assign freq_num_pass [2:0] = freq_num;
+	assign seq_num_pass [5:0] = seq_num;
+	assign rom_addr_pass = rom_addr;
 	
-	bcd_conv seq(seq_num, HEX0, HEX1, temp0);
-	bcd_conv freq(freq_num, HEX2, temp1, temp2);
-	bcd_conv rom(rom_addr, HEX3, HEX4, HEX5);
+	bcd_conv seq(seq_num_pass, HEX0, HEX1, temp0);
+	bcd_conv freq(freq_num_pass, HEX2, temp1, temp2);
+	bcd_conv rom(rom_addr_pass, HEX3, HEX4, HEX5);
 
 endmodule
